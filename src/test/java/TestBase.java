623f16270db6935a2733902c5eb2233256f60a2f
Mateusz Tadla <mtadla@sii.pl>
2020-12-12 12:09:36 +0100

    update

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,2 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class TestBase {
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "src\\main\\resources\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.quit();
+    }
 }
