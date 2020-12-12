d7aef6ee0111b3b050ce80b3cede8ca3720d8dc2
Mateusz Tadla <mtadla@sii.pl>
2020-12-12 11:29:01 +0100

    add proj

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,43 @@
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void thisTestShouldPass() {
+        System.out.println("This is my first test");
+        Assert.assertEquals(5, 5);
+    }
+
+    @Test
+    public void thisTestShouldPassToo() {
+        System.out.println("This is my second test");
+    }
+
+    @Test
+    public void thisTestShouldFail() {
+        System.out.println("This is my third test");
+        Assert.assertEquals(5, 10);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Uruchomilem sie przed kazdym testem!");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("Uruchomilem sie po kazdym tescie!");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Uruchomilem sie RAZ przed ");
+    }
+
+    @AfterClass
+    public void afterClass() throws Exception {
+        System.out.println("Uruchomilem sie RAZ po");
+        throw new Exception();
+    }
+}
