ccb1e2e963b19fae18af94f61940dafc01b6895f
Mateusz Tadla <mtadla@sii.pl>
2020-12-13 12:52:45 +0100

    update

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -20,11 +20,65 @@ public class FormPage {
     @FindBy(id = "inputLastName3")
     private WebElement lastName;
 
+    @FindBy(id = "inputEmail3")
+    private WebElement emailInput;
+
+    @FindBy(id = "inputAge3")
+    private WebElement ageInput;
+
+    @FindBy(id = "gridRadiosMale")
+    private WebElement maleRadioButton;
+
     @FindBy(name = "gridRadiosExperience")
     private List<WebElement> yearsOfExperience;
 
+    @FindBy(name = "gridCheckboxProfession")
+    private List<WebElement> professions;
+
     @FindBy(id = "selectContinents")
     private WebElement continents;
+
+    @FindBy(id = "selectSeleniumCommands")
+    private WebElement seleniumCommands;
+
+    @FindBy(id = "chooseFile")
+    private WebElement chooseFile;
+
+    @FindBy(className = "btn-primary")
+    private WebElement sendFormButton;
+
+    @FindBy(id = "validator-message")
+    private WebElement validatorMessage;
+
+    public void setEmail(String email) {
+        emailInput.sendKeys(email);
+    }
+
+    public void setAge(int age) {
+        // int: 15 -> string: "15"
+        ageInput.sendKeys(String.valueOf(age));
+    }
+
+    public void selectMale() {
+        maleRadioButton.click();
+    }
+
+    public void selectSeleniumCommand(String seleniumCommandValue) {
+        Select seleniumCommandSelect = new Select(seleniumCommands);
+        seleniumCommandSelect.selectByValue(seleniumCommandValue);
+    }
+
+    public void uploadFile(String absolutePath) {
+        chooseFile.sendKeys(absolutePath);
+    }
+
+    public void sendForm() {
+        sendFormButton.click();
+    }
+
+    public String getValidatorMessage() {
+        return validatorMessage.getText();
+    }
 
     public void setFirstName(String firstName) {
         firstNameInput.sendKeys(firstName);
@@ -35,10 +89,15 @@ public class FormPage {
     }
 
     public void selectRandomExperience() {
-        Random rnd = new Random();
-        int randomIndex = rnd.nextInt(yearsOfExperience.size());
-        WebElement randYearElement = yearsOfExperience.get(randomIndex);
-        randYearElement.click();
+        getRandomElement(yearsOfExperience).click();
+    }
+
+    public void selectRandomProfession() {
+        getRandomElement(professions).click();
+    }
+
+    private WebElement getRandomElement(List<WebElement> elements){
+        return elements.get(new Random().nextInt(elements.size()));
     }
 
     public void selectContinent(String continentValue) {
--- src/test/java/FormPopTest.java
+++ src/test/java/FormPopTest.java
@@ -1,15 +1,28 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormPage;
+
+import java.io.File;
 
 public class FormPopTest extends TestBase {
     @Test
     public void shouldFillFormWIthSuccess() {
         FormPage formPage = new FormPage(getDriver());
+        File file = new File("src\\main\\resources\\file.txt");
         getDriver().get("https://seleniumui.moderntester.pl/form.php");
 
         formPage.setFirstName("Jan");
         formPage.setLastName("Kowalski");
+        formPage.setEmail("j.k@wp.pl");
+        formPage.setAge(40);
+        formPage.selectMale();
+        formPage.selectRandomProfession();
         formPage.selectRandomExperience();
+        formPage.uploadFile(file.getAbsolutePath());
         formPage.selectContinent("south-america");
+        formPage.selectSeleniumCommand("switch-commands");
+        formPage.sendForm();
+
+        Assert.assertEquals(formPage.getValidatorMessage(), "Form send with success");
     }
 }
